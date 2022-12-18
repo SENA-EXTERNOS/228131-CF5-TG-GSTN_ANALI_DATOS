291057735a4ba010e3a7be8c1eb3ff9aa6cca7b4
Olimpo Macea <olimpo3m@gmail.com>
2022-12-18 09:17:11 -0500

    Avance tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,28 +7,296 @@
         span 2
       h1 Herramientas y técnicas para recolección de requisitos
 
-    
+    .tarjeta.color-tar-2.mb-5
+      .row.justify-content-around.align-items-center
+        .col-8.col-sm-5.col-md-4.mb-4.mb-md-0
+          img(src="@/assets/curso/temas/tema2/img-1.svg")
+        .col-md.col-lg-8
+          p.ms-2.me-4 La definición de requisitos identifica los aspectos clave del <em>software</em> y descarta los irrelevantes, es decir, los que no agregan valor. A continuación, se revisan algunas técnicas para analizar requerimientos como FURPS+ y MoSCoW, el prototipado como un modelo inicial, las reuniones JAD (Desarrollo Conjunto de Aplicaciones) como práctica del grupo de desarrollo que busca en varios días concretar las necesidades del sistema a desarrollar.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido.pb-0
+          p.mb-0.text-small <b>Figura 10.</b> Comic sobre las reuniones para definir requerimientos
+        figure
+          img.mb-2(src='@/assets/curso/temas/tema2/img-2.svg')
+          figcaption.color-caption-1 Nota. Adaptada y traducida de Dilbert Comics (2006), de Adams, S. (29 de enero de 2006)
+
+    p.mb-5 La adquisición y la comprensión de los requerimientos no es un proceso fácil, en ocasiones se presentan situaciones como las siguientes:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        figure
+          img.mb-2(src='@/assets/curso/temas/tema2/img-3.svg')
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido.pb-0
+          p.mb-0.text-small <b>Figura 11.</b> Entrevista, la técnica más utilizada
+        figure
+          img.mb-2(src='@/assets/curso/temas/tema2/img-4.svg')
+          figcaption.color-caption-1 Nota. Adaptada de Entrevistas eficaces para el trabajo de requerimientos. Fatto consultorías y sistemas (s.f.) [Video] 
 
     separador 
 
     #t_2_1.titulo-segundo.color-acento-contenido.pb-2
       h3 2.1 Entrevistas con patrocinador y usuarios
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-2
+        figure
+          img(src="@/assets/curso/temas/tema2/img-5.svg")
+      .col-lg-9
+        p Las entrevistas son la técnica más utilizada en todo proceso de desarrollo de software, porque es una forma de comunicación natural entre personas.
+
+    .tarjeta.color-slider.p-4
+      SlyderA(tipo="b")
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            p.ms-3 La técnica de entrevista se desarrolla en tres fases: preparación, realización y análisis. Una entrevista no se improvisa, previamente se deben hacen tareas como:
+            ul.lista-ul.ms-5
+              li 
+                <i class="fas fa-angle-right" aria-hidden="true"></i>
+                p Estudiar el dominio del problema
+              li 
+              li 
+                <i class="fas fa-angle-right" aria-hidden="true"></i>
+                p Seleccionar a las personas clave dentro de los interesados y ajustar sus agendas
+              li 
+              li 
+                <i class="fas fa-angle-right" aria-hidden="true"></i>
+                p Delimitar el objetivo y contenido
+              li 
+              li 
+                <i class="fas fa-angle-right" aria-hidden="true"></i>
+                p Planificar la logística
+              li 
+
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img-6.svg')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            p.ms-3 Las entrevistas son un intercambio de información verbal entre personas y pueden ser individuales o colectivas. Las preguntas, previamente escritas, se formulan de manera formal o informal; si el entrevistador tiene experiencia, se pueden hacer de manera espontánea. Las respuestas se utilizan para derivar requisitos.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img-7.svg')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            p.ms-3 El análisis de documentación consiste en obtener la información sobre los requerimientos a partir de documentos que ya están elaborados, por ejemplo, manuales del <em>software</em> que se está usando. Esta técnica es útil cuando no hay expertos en la materia o los entrevistados salieron de la organización. 
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img-8.svg')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            p.ms-3 Las mesas de trabajo <em>(Workshops)</em> se utilizan para determinar, establecer prioridades y hacer cierre de los requisitos para el desarrollo del <em>software</em>. Se enfocan en usuarios claves y expertos seleccionados, se realizan durante un período corto, pero intensivo; se usan para generar ideas de nuevas características o productos, resolver conflictos y llegar a un consenso, o para revisar los requisitos. 
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img-9.svg')
+
     separador 
 
     #t_2_2.titulo-segundo.color-acento-contenido.pb-2
       h3 2.2 Técnicas de creatividad grupal - Lluvia de ideas
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-3
+        figure
+          img(src="@/assets/curso/temas/tema2/img-10.svg")
+      .col-lg-9
+        p La lluvia de ideas es una actividad grupal de creatividad, usada para generar y recopilar ideas relacionadas con un mismo tema, en un ambiente libre de críticas o juicios. En la obtención de requisitos, se utiliza para profundizar el detalle sobre el tema. En la sesión se intercambian ideas sin orden ni filtro y luego se valoran. Sus fases son:
+        ul.lista-ul.ms-5.color-icono
+          li 
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+            p Preparación
+          li 
+          li 
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+            p Generación
+          li 
+          li 
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+            p Consolidación (revisar, descartar y priorizar ideas)
+          li 
+          li 
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+            p Documentación
+          li 
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido.pb-0
+          p.mb-0.text-small <b>Figura 12.</b> Lluvia de ideas, creatividad en libertad
+        figure
+          img.mb-2(src='@/assets/curso/temas/tema2/img-11.svg')
+          figcaption.color-caption-1 Nota. Reproducida de Reglas de la lluvia de ideas. Hansen (2021).
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-2
+        figure
+          img(src="@/assets/curso/temas/tema2/img-12.svg")
+      .col-lg-9
+        p La capacidad de crear nuevas ideas es una potencialidad desde la infancia. Por lo general, el primer paso en la lluvia de ideas es que todos se reúnan en un lugar tranquilo y comiencen a pensar (Hansen, 2021). Por su naturaleza libre y espontánea, la lluvia de ideas tiene cuatro características:
+
+    .bg--img_03
+      .row.justify-content-center.mb-4
+        .col-lg-4
+          figure(data-aos="zoom-in")
+            img(src="@/assets/curso/temas/tema2/img-13.png").img400.m-auto
+        .col-lg-8
+          AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta acordeonA")
+            .row(titulo="Cantidad antes que calidad")
+              .col-12
+                p.mb-0 Como se busca recoger tantas ideas como sea posible, es importante que estas fluyan libremente, aunque al final muchas se desechen.
+
+            .row(titulo="No a las críticas, discusiones o comentarios durante la sesión")
+              .col-12
+                p.mb-0 Esta regla debe ser respetada en todo momento para que el flujo de ideas no se perturbe o detenga.
+
+            .row(titulo="Registrar todas las ideas")
+              .col-12
+                p.mb-0 Que la sesión pase a la siguiente fase de evaluación sin haber sido censurada o filtrada. Ignorar ideas desmotiva a los participantes para seguir contribuyendo.
+
+            .row(titulo="Pensar con originalidad e inspirarse mutuamente")
+              .col-12
+                p.mb-0 Como surgen muchas ideas independientes, otras nuevas surgen a partir de ellas.
+
+    .row.justify-content-center.align-items-center
+      .col-lg-10
+        .bloque-texto-a.color-secundario.p-md-3.mb-5.cuadro-derecha-2
+          .row.m-0.align-items-center.justify-content-between.bloque-texto-a.texto-2
+            .col-lg-4.mb-1.mb-lg-0
+              figure.posicion_imagen_2
+                img(src='@/assets/curso/temas/tema2/img-14.svg')
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p Otra técnica es el Diseño Conjunto de Aplicaciones <em>(JAD)</em> como alternativa a las entrevistas individuales. Ayuda a los clientes y usuarios a formular problemas y a explorar soluciones, involucrándolos dentro del desarrollo, mediante talleres colaborativos.
+
+    p.mb-5 El <em>JAD</em> lleva a menores tiempos de desarrollo y mayor satisfacción del cliente involucrado durante todo el proceso. Tiene cuatro principios: dinámicas grupales, comunicación visual (diagramas, transparencias, multimedia, herramientas CASE), un proceso organizado y racional y la documentación tipo WYSIWYG <em>(what you see is what you get, lo que se ve es lo que se obtiene).</em> Durante las sesiones se trabajan los documentos a manera de diseño en fases.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        ImagenInfografica.color-acento-contenido.info-tema-2
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/temas/tema2/img-15.svg')
+
+          .tarjeta.color-acento-botones.p-3(x="37%" y="22%" numero="1")
+            .h5.mb-2 Adaptación
+            p Establece un equipo de trabajo identificando los componentes y responsabilidades, buscando consenso entre las necesidades de los usuarios y los servicios del <em>software</em> en producción.
+
+          .tarjeta.color-acento-botones.p-3(x="62%" y="43%" numero="2")
+            .h5.mb-2 La sesión <em>JAD</em> consiste en 
+            p Presentación, definición de objetivos y requisitos, documentación de temas abiertos y conclusión. Las sesiones pueden ser largas; durante estas se elaboran modelos empleando diagramas fáciles de entender, elaborados mediante herramientas CASE <em>(Computer Aided Software Engineering).</em>
+
+          .tarjeta.color-acento-botones.p-3(x="68%" y="25%" numero="3")
+            .h5.mb-2 Para concluir se completa, revisa y valida la documentación
+            p El producto final es un conjunto de modelos para ser aprobados por los participantes.
+
     separador 
 
     #t_2_3.titulo-segundo.color-acento-contenido.pb-2
       h3 2.3 Cuestionarios y encuestas
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/temas/tema2/img-16.svg')
+      .col-lg-8
+        p Los cuestionarios o encuestas son un conjunto de preguntas orientadas a obtener información específica de los usuarios. Se solicitan anónimamente y las respuestas se analizan y catalogan de forma cualitativa o cuantitativa. Los resultados sirven para tomar decisiones por parte de quienes solicitaron la información. 
+
+        p Las preguntas formuladas pueden ser cerradas o abiertas.
+
+        ul.lista-ul
+          li 
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+            p <b>Cerradas:</b> de selección múltiple entre varias alternativas. Estas preguntas deben ser objetivas con respuestas predefinidas, su evaluación es rápida y cuantitativa.
+          li 
+          li 
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+            p <b>Abiertas:</b> el encuestado responde la pregunta con sus propias palabras, pues tiene libertad de expresión, lo cual permite obtener información más detallada. Las respuestas son más difíciles de clasificar porque son cualitativas.
+          li 
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10   
+        .cajon.p-4.mb-4.color-cajon-1.mb-5
+          p En Tabla 4 se muestran ambos tipos de preguntas. En las cerradas se asignan puntajes para que cada usuario escoja, lo que permite que sean analizadas cuantitativamente. En las respuestas abiertas, el usuario puede responder en cada caso lo que considere desde su perspectiva.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        .titulo-sexto.color-acento-contenido.pb-0
+          p.mb-0.text-small <b>Tabla 4.</b> Tipos de pregunta en los cuestionarios
+        figure
+          img.mb-2(src='@/assets/curso/temas/tema2/img-17.svg')
+          figcaption.color-caption-1 Nota. Adaptada de Análisis y requerimientos de <em>software</em>: Manual autoformativo interactivo. Wong, S. (2017).
+
+
+
     separador 
 
     #t_2_4.titulo-segundo.color-acento-contenido.pb-2
       h3 2.4 Análisis de tareas y de dominio
-    separador 
 
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-3
+        figure
+          img(src='@/assets/curso/temas/tema2/img-18.svg') 
+      .col-lg-7
+        .cajon-b.color-cajon-25.p-3.mb-5.color-pos-3
+          p.mb-0 El análisis de requerimientos ejecuta las tareas que determinan las necesidades o condiciones que debe cumplir el <em>software</em>, ya sea un nuevo diseño o una versión de algo existente, teniendo en cuenta los conflictos entre requisitos de los diferentes roles. Analizar, documentar, validar y administrar los requisitos se hace en la fase de análisis.
+
+          p El análisis consiste en determinar si el requerimiento obtenido es claro, completo, conciso, no está duplicado, es válido, consistente y sin ambigüedades, resuelve conflictos y añade valor al proyecto. El proceso ha identificado las necesidades de los interesados, las implicaciones del <em>software</em>, representado los casos e historias de usuario, observado el sitio de trabajo y generado una cantidad de requisitos,
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido.pb-0
+          p.mb-0.text-small <b>Figura 13.</b> Esquema del modelo FURPS+
+        figure
+          img.mb-2(src='@/assets/curso/temas/tema2/img-19.svg')
+          figcaption.color-caption-1 Nota. Adaptada de FURPS+.QualidadeBR (10 de julio de 2008). 
+
+    .row.justify-content-center.align-items-center
+      .col-lg-10
+        .bloque-texto-a.color-secundario.p-md-3.mb-5.cuadro-derecha-2
+          .row.m-0.align-items-center.justify-content-between.bloque-texto-a.texto-2
+            .col-lg-4.mb-1.mb-lg-0
+              figure.posicion_imagen_2
+                img(src='@/assets/curso/temas/tema2/img-20.svg')
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p Para clasificar los requerimientos se usa la técnica FURPS+, <em>(Functionability, Usability, Reliability, Performance & Supportability):</em> funcionalidad, facilidad de uso, confiabilidad, rendimiento y facilidad de soporte. El modelo clasifica dos categorías: requisitos funcionales (F) que especifican las funciones que el <em>software</em> debe hacer y requerimientos no-funcionales (URPS), que puntualizan atributos del sistema o del medio ambiente del sistema. El símbolo (+) representa restricciones no-funcionales que deben tenerse en cuenta, como diseño, implementación, legales e interfase y los que se derivan de la norma ISO-9126 como eficiencia, mantenibilidad y portabilidad.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        .titulo-sexto.color-acento-contenido.pb-0
+          p.mb-0.text-small <b>Tabla 5.</b> Restricciones del modelo FURPS+
+        figure
+          img.mb-2(src='@/assets/curso/temas/tema2/img-22.svg')
+          figcaption.color-caption-1 Nota. Muestra los tipos de requerimientos del modelo de calidad de FURPS y descripción de cada uno de ellos. CVUDES (2015)
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-2
+        figure
+          img(src="@/assets/curso/temas/tema2/img-23.svg")
+      .col-lg-10
+        p Una vez clasificados, según el tipo, los requisitos se priorizan con el método MoSCoW que sirve para saber qué debe hacer primero, qué puede hacer después y qué no necesita hacer. El método corresponde a cuatro categorías:
+
+    separador 
     #t_2_5.titulo-segundo.color-acento-contenido.pb-2
       h3 2.5 Fabricación del modelo prototipo
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-3
+        figure
+          img(src='@/assets/curso/temas/tema2/img-21.svg') 
+      .col-lg-7
+        .cajon-b.color-cajon-25.p-3.mb-5.color-pos-4
+          p.mb-0 Utilizar prototipos como técnica de validación de requisitos para mostrarle al usuario una versión aproximada del  <em>software</em>, permite que explore y participe en la validación de la apariencia, funcionalidad y hacerse una idea de cómo quedará. Pueden ser interfases de usuario mediante maquetación, las pantallas gráficas y de aplicaciones <em>web.</em>
+
+
+
     separador 
 
     #t_2_6.titulo-segundo.color-acento-contenido.pb-2
